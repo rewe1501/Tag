a68c985bde7cfe47020c09932729106db26343d4
Rewe <140919868+mabrul@users.noreply.github.com>
2024-03-11 18:30:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 TOKEN = os.getenv("TOKEN")
+
+# Images
+START_IMG_URL = getenv(
+    "START_IMG_URL"https://telegra.ph/file/f6665b3a39ca04218f7ea.jpg"
+)
+ sys.exit()
